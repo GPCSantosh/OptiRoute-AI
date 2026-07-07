3cdecb92025d9e943123cfbaf59ef4aebedd2216
GPCSantosh <santoshnithin7@gmail.com>
2026-07-07 20:10:31 +0530

    Test Google Maps API key

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import AppRouter from "./router/AppRouter";
 
 function App() {
-  return <AppRouter />;
+    useEffect(() => {
+        console.log(
+            "Google Maps API Key:",
+            import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+        );
+    }, []);
+
+    return <AppRouter />;
 }
 
 export default App;

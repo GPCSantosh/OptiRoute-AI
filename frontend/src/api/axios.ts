b4c8b2e0f8074ba1074a8ab17bcf43f7cfef595c
GPCSantosh <santoshnithin7@gmail.com>
2026-07-24 15:34:04 +0530

    Checking for Errors

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
 });
+
+console.log("API URL =", import.meta.env.VITE_API_URL);
 
 export default api;
